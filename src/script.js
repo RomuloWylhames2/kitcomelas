07ab8213bcdc848a332900cf25b870442e8cf167
RomuloWylhames2 <blackwylhames1001@gmail.com>
2021-11-08 16:36:56 -0300

    Adding gallery

--- src/script.js
+++ src/script.js
@@ -44,3 +44,19 @@ window.onscroll = function(){
         nav.classList.remove('active');
     }
 }
+
+// GALERYA
+
+const gM = document.querySelector(".galeria-modal");
+const iGM = document.querySelector(".galeria-modal img");
+
+function fecharGaleria(){
+    gM.style.visibility = "hidden";
+    iGM.style.transform = "scale(0)";
+}
+
+function abrirGaleria(src){
+    gM.style.visibility = "visible";
+    iGM.style.transform = "scale(1)";
+    iGM.src = src
+}
